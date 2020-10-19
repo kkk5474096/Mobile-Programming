84453067979c6a0799f40efbc153baae8bf1c979
leekangmin <kkk5474096@naver.com>
2020-10-19 18:33:52 +0900

    6장

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,129 +1,58 @@
-package com.example.mobileprogramming;
+package com.example.myapplication;
 
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvName, tvEmail;
-    Button button1, button2;
-    EditText dlgEdtName, dlgEdtEmail;
-    TextView toastText;
-    View dialogView, toastView;
-    LinearLayout baseLayout;
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("사용자 정보 입력");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.web);
 
-        tvName = (TextView) findViewById(R.id.tvName);
-        tvEmail = (TextView) findViewById(R.id.tvEmail);
-        button1 = (Button) findViewById(R.id.button1);
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
 
-        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        web.setWebViewClient(new CookWebViewClient());
 
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
 
-
-        button1.setOnClickListener(new View.OnClickListener() {
-
+        btnGo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                dialogView = (View) View.inflate(MainActivity.this,
-                        R.layout.dialog1, null);
+                web.loadUrl(edtUrl.getText().toString());
+            }
+        });
 
-                AlertDialog.Builder dlg = new AlertDialog.Builder(
-                        MainActivity.this);
-                dlg.setTitle("사용자 정보 입력");
-                dlg.setIcon(R.drawable.people);
-                dlg.setView(dialogView);
-                dlg.setPositiveButton("확인",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-                                dlgEdtName = (EditText) dialogView
-                                        .findViewById(R.id.dlgEdt1);
-                                dlgEdtEmail = (EditText) dialogView
-                                        .findViewById(R.id.dlgEdt2);
-
-                                tvName.setText(dlgEdtName.getText().toString());
-                                tvEmail.setText(dlgEdtEmail.getText()
-                                        .toString());
-                                // dlgLayout = null;
-                            }
-                        });
-
-                dlg.setNegativeButton("취소",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-                                Toast toast = new Toast(MainActivity.this);
-
-                                toastView = (View) View.inflate(
-                                        MainActivity.this,
-                                        R.layout.toast1, null);
-                                toastText = (TextView) toastView
-                                        .findViewById(R.id.toastText1);
-                                toastText.setText("취소했습니다");
-
-                                int xOffset = (int) (Math.random() * getDisplay().getWidth());
-                                int yOffset = (int) (Math.random() * getDisplay().getHeight());
-
-                                toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
-                                toast.setView(toastView);
-                                toast.show();
-                            }
-                        });
-                dlg.show();
-
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.goBack();
             }
         });
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        super.onCreateOptionsMenu(menu);
-        MenuInflater mInflater = getMenuInflater();
-        mInflater.inflate(R.menu.menu1, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.itemRed:
-                baseLayout.setBackgroundColor(Color.RED);
-                return true;
-            case R.id.itemGreen:
-                baseLayout.setBackgroundColor(Color.GREEN);
-                return true;
-            case R.id.itemBlue:
-                baseLayout.setBackgroundColor(Color.BLUE);
-                return true;
+    class CookWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
         }
-        return false;
     }
+
 }
-
-
-
-
