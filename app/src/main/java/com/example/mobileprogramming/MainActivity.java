800542a64ec878f9079530b608e192750c400561
leekangmin <kkk5474096@naver.com>
2020-11-27 15:38:13 +0900

    12장

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -2,84 +2,134 @@ package com.example.mobileprogramming;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
+
+    myDBHelper myHelper;
+    EditText edtName, edtNumber, edtNameResult, edtNumberResult;
+    Button btnInit, btnInsert, btnUpdate, btnDelete, btnSelect;
+    SQLiteDatabase sqlDB;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("갤러리 영화 포스터");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.web);
+        setTitle("가수 그룹 관리 DB (수정)");
 
-        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtNumber = (EditText) findViewById(R.id.edtNumber);
+        edtNameResult = (EditText) findViewById(R.id.edtNameResult);
+        edtNumberResult = (EditText) findViewById(R.id.edtNumberResult);
 
-        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
-        gallery.setAdapter(galAdapter);
-    }
+        btnInit = (Button) findViewById(R.id.btnInit);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
 
-    public class MyGalleryAdapter extends BaseAdapter {
+        myHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB, 1, 2); // 인수는 아무거나 입력하면 됨.
+                sqlDB.close();
+            }
+        });
 
-        Context context;
-        Integer[] posterID = { R.drawable.mov1, R.drawable.mov2,
-                R.drawable.mov3, R.drawable.mov4, R.drawable.mov5,
-                R.drawable.mov6, R.drawable.mov7, R.drawable.mov8,
-                R.drawable.mov9, R.drawable.mov10 };
-        String[] posterName = { "반도", "아이언맨3",
-                "어벤져스", "어바웃타임", "인터스텔라",
-                "다크나이트 라이즈", "토르: 천둥의신", "부산행",
-                "괴물", "도둑들" };
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO groupTBL VALUES ( '"
+                        + edtName.getText().toString() + "' , "
+                        + edtNumber.getText().toString() + ");");
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(), "입력됨",
+                        Toast.LENGTH_SHORT).show();
+                btnSelect.callOnClick();
+            }
+        });
 
-        public MyGalleryAdapter(Context c) {
-            context = c;
-        }
-
-        public int getCount() {
-            return posterID.length;
-        }
-
-        public Object getItem(int arg0) {
-            return null;
-        }
-
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageview = new ImageView(context);
-            imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
-            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imageview.setPadding(5, 5, 5, 5);
-            imageview.setImageResource(posterID[position]);
-
-            final int pos = position;
-            imageview.setOnTouchListener(new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
-                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                    ivPoster.setImageResource(posterID[pos]);
-
-                    Toast toast = new Toast(MainActivity.this);
-                    View toastView = View.inflate(MainActivity.this, R.layout.toast1, null);
-                    TextView toastText = toastView.findViewById(R.id.toastText1);
-                    toastText.setText(posterName[pos]);
-                    toast.setView(toastView);
-                    toast.show();
-                    return false;
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                if (edtName.getText().toString() != "") {
+                    sqlDB.execSQL("UPDATE groupTBL SET gNumber ="
+                            + edtNumber.getText() + " WHERE gName = '"
+                            + edtName.getText().toString() + "';");
                 }
-            });
+                sqlDB.close();
 
-            return imageview;
+                Toast.makeText(getApplicationContext(), "수정됨",
+                        Toast.LENGTH_SHORT).show();
+                btnSelect.callOnClick();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                if (edtName.getText().toString() != "") {
+                    sqlDB.execSQL("DELETE FROM groupTBL WHERE gName = '"
+                            + edtName.getText().toString() + "';");
+
+                }
+                sqlDB.close();
+
+                Toast.makeText(getApplicationContext(), "삭제됨",
+                        Toast.LENGTH_SHORT).show();
+                btnSelect.callOnClick();
+            }
+        });
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;", null);
+
+                String strNames = "그룹이름" + "\r\n" + "--------" + "\r\n";
+                String strNumbers = "인원" + "\r\n" + "--------" + "\r\n";
+
+                while (cursor.moveToNext()) {
+                    strNames += cursor.getString(0) + "\r\n";
+                    strNumbers += cursor.getString(1) + "\r\n";
+                }
+
+                edtNameResult.setText(strNames);
+                edtNumberResult.setText(strNumbers);
+
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context) {
+            super(context, "groupDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE  groupTBL ( gName CHAR(20) PRIMARY KEY, gNumber INTEGER);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS groupTBL");
+            onCreate(db);
         }
     }
+
 }
