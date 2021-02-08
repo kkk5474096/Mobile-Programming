2a90a53ea3932dc8b9b285f53b85d7175e0eb749
kkk5474096 <kkk5474096@naver.com>
2021-02-08 22:59:49 +0900

    intent 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,27 +1,30 @@
 package com.example.mobileprogramming;
 
 
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnEnglish = (Button) findViewById(R.id.btn_english);
-        btnEnglish.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(),SubActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
+    public void buttonClicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://naver.com"));
+        startActivity(intent);
+
+
+    }
+
 }
