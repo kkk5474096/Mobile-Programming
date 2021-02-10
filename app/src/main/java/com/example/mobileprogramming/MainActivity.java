18f8f49ebd2d74078051adb6196c24635e589a46
leekangmin <kkk5474096@naver.com>
2021-02-10 14:18:11 +0900

    이벤트 처리 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -2,45 +2,106 @@ package com.example.mobileprogramming;
 
 
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
+
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView sun, duk;
-    int index = 0;
+    TextView textView;
+    GestureDetector detector;
+    View view1, view2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sun = (ImageView) findViewById(R.id.sun);
-        duk = (ImageView) findViewById(R.id.duk);
+        textView = (TextView) findViewById(R.id.text);
+        view1 = (View) findViewById(R.id.view1);
+        view2 = (View) findViewById(R.id.view2);
+
+        view1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    println("손가락 눌렸음 :" + curX + "," + curY);
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                    println("손가락 움직임 :" + curX + "," + curY);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    println("손가락 떼졌음 :" + curX + "," + curY);
+                }
+
+                return true;
+            }
+        });
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                println("onDown() 호출됨");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                println("onDown() 호출됨");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                println("onSingleTapUp() 호출됨");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onScroll() 호출됨" + v + "," + v1);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println("onLongPress() 호출됨");
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onFling() 호출됨" + v + "," + v1);
+                return true;
+            }
+        });
+
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                detector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+
+
 
     }
 
-    public void buttonClicked(View v) {
-            index += 1;
-            if (index > 1) {
-                index = 0;
-            }
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
 
-            if (index == 0) {
-                sun.setVisibility(View.VISIBLE);
-                duk.setVisibility(View.INVISIBLE);
-            } else if (index == 1) {
-                sun.setVisibility(View.INVISIBLE);
-                duk.setVisibility(View.VISIBLE);
-            }
-        }
 
     }
 
