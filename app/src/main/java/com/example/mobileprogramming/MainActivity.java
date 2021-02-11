1bcc06fe45c3b1a2a1f630a8486b5f8a1ef56334
leekangmin <kkk5474096@naver.com>
2021-02-11 22:29:44 +0900

    대화상자 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -5,103 +5,62 @@ package com.example.mobileprogramming;
 
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+
     TextView textView;
-    GestureDetector detector;
-    View view1, view2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.text);
-        view1 = (View) findViewById(R.id.view1);
-        view2 = (View) findViewById(R.id.view2);
-
-        view1.setOnTouchListener(new View.OnTouchListener() {
+        Button button = (Button) findViewById(R.id.button1);
+        textView = (TextView) findViewById(R.id.textview);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-
-                float curX = motionEvent.getX();
-                float curY = motionEvent.getY();
-
-                if (action == MotionEvent.ACTION_DOWN) {
-                    println("손가락 눌렸음 :" + curX + "," + curY);
-                } else if (action == MotionEvent.ACTION_MOVE) {
-                    println("손가락 움직임 :" + curX + "," + curY);
-                } else if (action == MotionEvent.ACTION_UP) {
-                    println("손가락 떼졌음 :" + curX + "," + curY);
-                }
-
-                return true;
-            }
-        });
-
-        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent motionEvent) {
-                println("onDown() 호출됨");
-                return true;
-            }
-
-            @Override
-            public void onShowPress(MotionEvent motionEvent) {
-                println("onDown() 호출됨");
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent motionEvent) {
-                println("onSingleTapUp() 호출됨");
-                return true;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                println("onScroll() 호출됨" + v + "," + v1);
-                return true;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent motionEvent) {
-                println("onLongPress() 호출됨");
-
-            }
-
-            @Override
-            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                println("onFling() 호출됨" + v + "," + v1);
-                return true;
-            }
-        });
-
-        view2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                detector.onTouchEvent(motionEvent);
-                return true;
+            public void onClick(View view) {
+                showMessage();
             }
         });
 
 
 
     }
+    public void showMessage() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("안내");
+        builder.setIcon(R.drawable.ic_launcher_foreground);
+        builder.setMessage("종료하시겠습니까?");
 
-    public void println(String data) {
-        textView.append(data + "\n");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Snackbar.make(textView, "예 버튼이 눌렸습니다.", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Snackbar.make(textView, "아니오 버튼이 눌렸습니다.", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        builder.show();
     }
-
-
-    }
-
+}
