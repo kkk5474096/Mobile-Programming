07c5255c6892a8dc8a95e5db033b5cfdf176ec72
leekangmin <kkk5474096@naver.com>
2021-02-12 18:12:04 +0900

    Inflater 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.mobileprogramming;
 
 
-
-
-
-
-
+import android.content.Context;
 import android.os.Bundle;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,12 +15,27 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+    Button button;
+    FrameLayout frameLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.activity_sub, frameLayout, true);
+            }
+        });
+
+
 
 
     }
