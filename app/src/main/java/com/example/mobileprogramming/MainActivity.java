8e85d6f2dd6143f9adae645b1b39480ad38e7c5f
leekangmin <kkk5474096@naver.com>
2021-02-16 17:32:57 +0900

    intent 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,82 +1,43 @@
 package com.example.mobileprogramming;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.widget.Button;
 import android.widget.Toast;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    SingerAdapter adapter;
 
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridView = (GridView) findViewById(R.id.gridView);
-
-        adapter = new SingerAdapter();
-        adapter.addItem(new SingerItem("소녀시대", "010-1000-1000", R.drawable.duk));
-        adapter.addItem(new SingerItem("걸스데이", "010-2000-2000", R.drawable.sun));
-        adapter.addItem(new SingerItem("여자친구", "010-3000-3000", R.drawable.kim));
-        adapter.addItem(new SingerItem("티아라", "010-4000-4000", R.drawable.web));
-        adapter.addItem(new SingerItem("애프터스쿨", "010-5000-5000", R.drawable.iu));
-
-
-        gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                SingerItem item = (SingerItem) adapter.getItem(i);
-                Toast.makeText(getApplicationContext(), "선택 : " + item.getName(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivityForResult(intent, 102);
             }
         });
-
     }
 
-    class SingerAdapter extends BaseAdapter {
-        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        @Override
-        public int getCount() {
-            return items.size();
+        if (requestCode == 102) {
+            String name = data.getStringExtra("name");
+            Toast.makeText(getApplicationContext(), "메뉴화면으로부터의 이름 : " + name, Toast.LENGTH_LONG).show();
         }
-
-        public void addItem(SingerItem item) {
-            items.add(item);
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return items.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            SingerItemView v = new SingerItemView(getApplicationContext());
-
-            SingerItem item = items.get(i);
-            v.setName(item.getName());
-            v.setMobile(item.getMobile());
-            v.setImage(item.getResId());
-            return v;
-        }
-
     }
 }
--- app/src/main/java/com/example/mobileprogramming/SubActivity.java
+++ app/src/main/java/com/example/mobileprogramming/SubActivity.java
@@ -6,12 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SubActivity extends Activity {
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+
+    Button button1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+
+            }
+        });
 
 
     }
