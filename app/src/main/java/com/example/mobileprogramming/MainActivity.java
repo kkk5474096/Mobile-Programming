ad4cea8e6cd2b0289d9fd4ca5cca205eda83576c
leekangmin <kkk5474096@naver.com>
2021-02-18 18:29:36 +0900

    LifeCycle, SharedPreferences 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.mobileprogramming;
 
 
-import android.content.Intent;
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,22 +22,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("MainActivity", "onCreate() 호출됨");
+
         button = (Button) findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
-
-                ArrayList<String> names = new ArrayList<String>();
-                names.add("김진수");
-                names.add("황수연");
-                intent.putExtra("names", names);
-
-                SimpleData data = new SimpleData(100,"Hello");
-                intent.putExtra("data", data);
-
-                startActivityForResult(intent, 102);
+                finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MainActivity", "onStart() 호출됨");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("MainActivity", "onStop() 호출됨");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("MainActivity", "onDestroy() 호출됨");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("MainActivity", "onPause() 호출됨");
+
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name", "소녀시대");
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("MainActivity", "onResume() 호출됨");
+
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        if( pref != null) {
+            String name = pref.getString("name" , "");
+
+            Log.d("MainActivity", "복구된 이름 : " + name);
+
+        }
+
     }
 }
