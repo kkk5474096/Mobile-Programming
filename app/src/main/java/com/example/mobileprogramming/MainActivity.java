e7669db399776f497a6a5841d4d583cedcbddadd
leekangmin <kkk5474096@naver.com>
2021-02-23 20:25:48 +0900

    Fragment & ImageView 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -6,45 +6,29 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    Button button1;
-    MainFragment fragment1;
-    MenuFragment fragment2;
 
+    ListFragment fragment1;
+    ViewerFragment fragment2;
 
+    FragmentManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button1 = (Button) findViewById(R.id.button1);
+        manager = getSupportFragmentManager();
 
-        fragment1 = new MainFragment();
-        fragment2 = new MenuFragment();
+        fragment1 = (ListFragment) manager.findFragmentById(R.id.listFragment);
+        fragment2 = (ViewerFragment) manager.findFragmentById(R.id.viewerFragment);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
-            }
-        });
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
-            }
-        });
 
     }
-    public void onFragmentChange(int index) {
-        if (index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
-        } else if (index == 1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
-        }
+
+    public void onImageChange(int index) {
+        fragment2.setImage(index);
+
     }
 }
---
+++ app/src/main/java/com/example/mobileprogramming/ViewerFragment.java
@@ -0,0 +1,31 @@
+package com.example.mobileprogramming;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ViewerFragment extends Fragment {
+    ImageView imageView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_viewer, container, false);
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        return rootView;
+    }
+    public void setImage(int index) {
+        if (index == 0) {
+            imageView.setImageResource(R.drawable.iu);
+        } else if (index == 1) {
+            imageView.setImageResource(R.drawable.kim);
+        } else if (index == 2) {
+            imageView.setImageResource(R.drawable.sun);
+        }
+    }
+}
