f294215954934ebce4c80bc268a2ec31dc4e611e
leekangmin <kkk5474096@naver.com>
2021-02-26 19:10:20 +0900

    NavigationDrawer & Fragment 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,73 +1,51 @@
 package com.example.mobileprogramming;
 
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
-
-import com.example.mobileprogramming.adapter.MoviePagerAdapter;
-import com.example.mobileprogramming.fragment.Fragment1;
-import com.example.mobileprogramming.fragment.Fragment2;
-import com.example.mobileprogramming.fragment.Fragment3;
-import com.google.android.material.tabs.TabLayout;
-
 
 public class MainActivity extends AppCompatActivity {
-    Fragment1 fragment1;
-    Fragment2 fragment2;
-    Fragment3 fragment3;
-    MoviePagerAdapter adapter;
-    TabLayout tabs;
-    ViewPager pager;
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        tabs = (TabLayout) findViewById(R.id.tabs);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_1, R.id.nav_2, R.id.nav_3)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
 
-        tabs.addTab(tabs.newTab().setText("친구"));
-        tabs.addTab(tabs.newTab().setText("동료"));
-        tabs.addTab(tabs.newTab().setText("연인"));
 
-        adapter = new MoviePagerAdapter(getSupportFragmentManager());
-
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setOffscreenPageLimit(3);
-
-        adapter.addItem(fragment1);
-        adapter.addItem(fragment2);
-        adapter.addItem(fragment3);
-
-        pager.setAdapter(adapter);
-
-        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                pager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
