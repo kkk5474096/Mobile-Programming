a251300498b04001fb169d73da41921da888def8
leekangmin <kkk5474096@naver.com>
2021-03-08 17:25:10 +0900

    Thread & AsyncTask 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mobileprogramming;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,37 +31,55 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread(new Runnable() {
-                    boolean running = false;
-                    int value = 0;
-
-                    public void run() {
-                        running = true;
-                        while (running) {
-                            value += 1;
-                            
-                            handler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    textView.setText("현재 값 : " + value);
-                                }
-                            });
-
-                            try {
-                                Thread.sleep(1000);
-                            } catch (Exception e) {
-                            }
-                        }
-                    }
-                }).start();
+                ValueTask task = new ValueTask();
+                task.execute("시작");
             }
         });
+//
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+    class ValueTask extends AsyncTask<String, Integer, Integer> {
+        int value = 0;
+        boolean running = false;
+        @Override
+        protected Integer doInBackground(String... strings) {
+            running = true;
+            while (running) {
+                if (value >= 10) {
+                    break;
+                }
+                value += 1;
+
+                publishProgress(value);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                }
             }
-        });
+            return value;
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            textView.setText("현재값 : " + value);
+        }
+
+        @Override
+        protected void onPostExecute(Integer s) {
+            super.onPostExecute(s);
+
+            Toast.makeText(getApplicationContext(), "완료됨.", Toast.LENGTH_LONG).show();
+        }
+
     }
 
 }
