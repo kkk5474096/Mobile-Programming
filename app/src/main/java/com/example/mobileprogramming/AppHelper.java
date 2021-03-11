3adbdd386982f24b7032e4fdc766ab15da68619d
leekangmin <kkk5474096@naver.com>
2021-03-11 16:44:43 +0900

    라이브러리 Volley & 웹 요청과 응답

---
+++ app/src/main/java/com/example/mobileprogramming/AppHelper.java
@@ -0,0 +1,7 @@
+package com.example.mobileprogramming;
+
+import com.android.volley.RequestQueue;
+
+public class AppHelper {
+    public static RequestQueue requestQueue;
+}
--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -13,10 +13,19 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -24,9 +33,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
-    EditText editText;
-    String urlStr;
-    Handler handler = new Handler();
+
 
 
     @Override
@@ -36,59 +43,52 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
-        editText = findViewById(R.id.editText);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                urlStr = editText.getText().toString();
-                RequestThread thread = new RequestThread();
-                thread.start();
+                sendRequest();
             }
         });
-    }
-    class RequestThread extends Thread {
-        public void run() {
-
-            try{
-                URL url = new URL(urlStr);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                if (conn != null) {
-                    conn.setConnectTimeout(10000);
-                    conn.setRequestMethod("GET");
-                    conn.setDoInput(true);
-                    conn.setDoOutput(true);
-
-                    int resCode = conn.getResponseCode();
-
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String line = null;
-
-                    while (true) {
-                        line = reader.readLine();
-                        if (line == null) {
-                            break;
-                        }
-                        println(line);
-                    }
-                    reader.close();
-                    conn.disconnect();
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (AppHelper.requestQueue == null) {
+            AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
     }
 
-    public void println(final String data) {
-        handler.post(new Runnable() {
+    public void sendRequest() {
+        String url = "https://www.google.co.kr";
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        println("응답 -> " + response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("에러 ->" + error.getMessage());
+                    }
+                }
+        ) {
             @Override
-            public void run() {
-                textView.append(data + "\r");
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
             }
-        });
+        };
+        request.setShouldCache(false);
+        AppHelper.requestQueue.add(request);
+        println("요청 보냄");
+    }
+
+
+    public void println(String data) {
+                textView.setText(data + "\n");
+            }
 
     }
-}
