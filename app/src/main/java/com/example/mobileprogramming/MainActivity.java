88c9e8c262a8a8da81fa79014eb4f82acf8398ee
Leekangmin <kkk5474096@naver.com>
2021-03-25 17:24:51 +0900

    사진 찍어 저장하기 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -2,25 +2,39 @@ package com.example.mobileprogramming;
 
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    TextView textView;
+    ImageView imageView;
+    File file;
 
 
     @Override
@@ -29,21 +43,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+
+        File sdcard = Environment.getExternalStorageDirectory();
+        file = new File(sdcard, "capture.jpg");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int status = NetworkStatus.getConnectivityStats(getApplicationContext());
-                if (status == NetworkStatus.TYPE_MOBILE) {
-                    textView.setText("모바일로 연결됨.");
-                } else if (status == NetworkStatus.TYPE_WIFI) {
-                    textView.setText("무선랜으로 연결됨.");
-                } else {
-                    textView.setText("연결 안됨");
-                }
+                capture();
             }
         });
+    }
+    public void capture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(Build.VERSION.SDK_INT>=24){
+            try{
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                builder.detectFileUriExposure();
+            }catch(Exception e){}
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent,101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( requestCode == 101 & resultCode == Activity.RESULT_OK) {
+            Log.d("test", "test");
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
 
