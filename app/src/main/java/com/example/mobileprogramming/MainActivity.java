4117264a2310970fb851b5f9aa9f87ba03f60dd6
Leekangmin <kkk5474096@naver.com>
2021-03-26 00:00:32 +0900

    사진 미리보기 & 저장하기

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,40 +1,20 @@
 package com.example.mobileprogramming;
 
 
-
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
+import android.hardware.Camera;
 import android.os.Bundle;
-
-import android.os.Environment;
-import android.os.StrictMode;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
-import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     ImageView imageView;
-    File file;
+    CameraSurfaceView surfaceView;
 
 
     @Override
@@ -44,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-
-        File sdcard = Environment.getExternalStorageDirectory();
-        file = new File(sdcard, "capture.jpg");
+        surfaceView = (CameraSurfaceView) findViewById(R.id.surfaceView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,30 +33,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void capture() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if(Build.VERSION.SDK_INT>=24){
-//            try{
-//                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-//                StrictMode.setVmPolicy(builder.build());
-//                builder.detectFileUriExposure();
-//            }catch(Exception e){}
-//        }
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this, "com.example.mobileprogramming.MainActivity", file));
-        startActivityForResult(intent,101);
-    }
+        surfaceView.capture(new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8;
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+                imageView.setImageBitmap(bitmap);
 
-        if( requestCode == 101 & resultCode == Activity.RESULT_OK) {
-            Log.d("test", "test");
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 8;
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-            imageView.setImageBitmap(bitmap);
-        }
+                camera.startPreview();
+            }
+        });
     }
 }
 
