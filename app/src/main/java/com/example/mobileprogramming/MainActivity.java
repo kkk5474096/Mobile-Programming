7bd5fc747cb91292c81fcf2e4bf604ca78622525
Leekangmin <kkk5474096@naver.com>
2021-03-26 14:54:47 +0900

    MediaPlayer 음악 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -4,18 +4,24 @@ package com.example.mobileprogramming;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    ImageView imageView;
-    CameraSurfaceView surfaceView;
-
+    Button button2;
+    Button button3;
+    Button button4;
+    MediaPlayer player;
+    int position = 0;
+    public static String url = "http://sites.google.com/site/ubiaccessmobile/sample_audio.amr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +29,86 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView);
-        surfaceView = (CameraSurfaceView) findViewById(R.id.surfaceView);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                capture();
+                playAudio();
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseAudio();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resumeAudio();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAudio();
+            }
+        });
+
     }
 
-    public void capture() {
-        surfaceView.capture(new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 8;
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+    public void playAudio() {
+        try {
+            closePlayer();
+            player = new MediaPlayer();
+            player.setDataSource(url);
+            player.prepare();
+            player.start();
 
-                imageView.setImageBitmap(bitmap);
+            Toast.makeText(this, "재생시작됨.", Toast.LENGTH_LONG).show();
 
-                camera.startPreview();
-            }
-        });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    public void pauseAudio() {
+        if (player != null) {
+            position = player.getCurrentPosition();
+            player.pause();
+
+            Toast.makeText(this, "일시정지됨.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void resumeAudio() {
+        if (player != null && !player.isPlaying()) {
+            player.seekTo(position);
+            player.start();
+
+            Toast.makeText(this, "재시작됨.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void stopAudio() {
+        if (player != null && player.isPlaying()) {
+            player.stop();
+
+            Toast.makeText(this, "중지됨.", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
