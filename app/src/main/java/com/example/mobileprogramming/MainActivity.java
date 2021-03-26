d38ebf5df0667559770bd96e838000fb6cc109e7
Leekangmin <kkk5474096@naver.com>
2021-03-27 00:22:49 +0900

    음성 녹음 & 재생

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -1,58 +1,171 @@
 package com.example.mobileprogramming;
 
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
+
 import android.media.MediaPlayer;
-import android.net.Uri;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    VideoView videoView;
-
-    public static String url = "http://sites.google.com/site/ubiaccessmobile/sample_video.mp4";
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    int position = 0;
+    MediaPlayer player;
+    MediaRecorder recorder;
+    String filename;
+    public static String url = "http://sites.google.com/site/ubiaccessmobile/sample_audio.amr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        File sdcard = Environment.getExternalStorageDirectory();
+        File file = new File(sdcard, "recorded.mp4");
+        filename = file.getAbsolutePath();
+        Log.d("test", "저장할 파일명 : " + filename);
+
         button = findViewById(R.id.button);
-        videoView = findViewById(R.id.videoView);
-
-        MediaController controller = new MediaController(this);
-        videoView.setMediaController(controller);
-        videoView.setVideoURI(Uri.parse(url));
-        videoView.requestFocus();
-
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                Toast.makeText(getApplicationContext(), "동영상 준비됨", Toast.LENGTH_LONG).show();
-            }
-        });
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                videoView.seekTo(0);
-                videoView.start();
+                recordAudio();
             }
         });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAudio();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseAudio();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resumeAudio();
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAudio();
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecording();
+            }
+        });
 
     }
+
+    public void recordAudio() {
+        recorder = new MediaRecorder();
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+
+        recorder.setOutputFile(filename);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+
+            Toast.makeText(this, "녹음시작됨.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopRecording() {
+        if (recorder != null) {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+
+            Toast.makeText(this, "녹음 중지됨.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void playAudio() {
+        try {
+            closePlayer();
+            player = new MediaPlayer();
+            player.setDataSource(filename);
+            player.prepare();
+            player.start();
+
+            Toast.makeText(this, "재생시작됨.", Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    public void pauseAudio() {
+        if (player != null) {
+            position = player.getCurrentPosition();
+            player.pause();
+
+            Toast.makeText(this, "일시정지됨.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void resumeAudio() {
+        if (player != null && !player.isPlaying()) {
+            player.seekTo(position);
+            player.start();
+
+            Toast.makeText(this, "재시작됨.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void stopAudio() {
+        if (player != null && player.isPlaying()) {
+            player.stop();
+
+            Toast.makeText(this, "중지됨.", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
 
