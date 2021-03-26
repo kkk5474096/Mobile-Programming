033fb67ac720024b4156c9e10848ad6ce92f5e50
Leekangmin <kkk5474096@naver.com>
2021-03-26 15:03:33 +0900

    MediaPlayer 동영상 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -5,23 +5,23 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    Button button2;
-    Button button3;
-    Button button4;
-    MediaPlayer player;
-    int position = 0;
-    public static String url = "http://sites.google.com/site/ubiaccessmobile/sample_audio.amr";
+    VideoView videoView;
+
+    public static String url = "http://sites.google.com/site/ubiaccessmobile/sample_video.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,86 +29,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
+        videoView = findViewById(R.id.videoView);
+
+        MediaController controller = new MediaController(this);
+        videoView.setMediaController(controller);
+        videoView.setVideoURI(Uri.parse(url));
+        videoView.requestFocus();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                Toast.makeText(getApplicationContext(), "동영상 준비됨", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playAudio();
+                videoView.seekTo(0);
+                videoView.start();
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pauseAudio();
-            }
-        });
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resumeAudio();
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopAudio();
-            }
-        });
-
-    }
-
-    public void playAudio() {
-        try {
-            closePlayer();
-            player = new MediaPlayer();
-            player.setDataSource(url);
-            player.prepare();
-            player.start();
-
-            Toast.makeText(this, "재생시작됨.", Toast.LENGTH_LONG).show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void closePlayer() {
-        if (player != null) {
-            player.release();
-            player = null;
-        }
-    }
-
-    public void pauseAudio() {
-        if (player != null) {
-            position = player.getCurrentPosition();
-            player.pause();
-
-            Toast.makeText(this, "일시정지됨.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void resumeAudio() {
-        if (player != null && !player.isPlaying()) {
-            player.seekTo(position);
-            player.start();
-
-            Toast.makeText(this, "재시작됨.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void stopAudio() {
-        if (player != null && player.isPlaying()) {
-            player.stop();
-
-            Toast.makeText(this, "중지됨.", Toast.LENGTH_LONG).show();
-        }
     }
 }
 
