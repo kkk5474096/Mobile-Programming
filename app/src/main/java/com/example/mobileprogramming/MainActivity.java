2adf436c91ceaba56b80c2f08533d4e80a13b66a
Leekangmin <kkk5474096@naver.com>
2021-04-01 16:56:30 +0900

    페이지 슬라이딩 활용

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -6,26 +6,66 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    LinearLayout page;
+    Animation translateLeft;
+    Animation translateRight;
+    boolean isPageOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        translateLeft = AnimationUtils.loadAnimation(this, R.anim.translate_left);
+        translateRight = AnimationUtils.loadAnimation(this, R.anim.translate_right);
+
+        SlidingAnimationListener listener = new SlidingAnimationListener();
+        translateLeft.setAnimationListener(listener);
+        translateRight.setAnimationListener(listener);
+
         button = findViewById(R.id.button);
+        page = findViewById(R.id.page);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Animation scale = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
-                v.startAnimation(scale);
+                if (isPageOpen) {
+                    page.startAnimation(translateRight);
+
+                } else {
+                    page.setVisibility(View.VISIBLE);
+                    page.startAnimation(translateLeft);
+                }
             }
         });
+    }
+    class SlidingAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
 
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            if (isPageOpen) {
+                page.setVisibility(View.INVISIBLE);
+                button.setText("열기");
+                isPageOpen = false;
+            } else {
+                button.setText("닫기");
+                isPageOpen = true;
+            }
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
 
